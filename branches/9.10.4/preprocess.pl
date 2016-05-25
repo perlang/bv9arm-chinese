f9bc4d5cd2c6a6db6a3f137fce48fffe1aa8481c
perlang <sunguonian@gmail.com>
2016-05-25 22:22:37 +0800

    add 9.10.4

---
+++ branches/9.10.4/preprocess.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+
+# preprocess.pl
+
+undef($/);
+
+$_ = <>;
+
+# comment "\newif\ifpdf" and add \RequirePackage{ifpdf}
+s/(\\newif\\ifpdf)\n(\\ifx\\pdfoutput\\undefined)\n(\\pdffalse % we are not running PDFLaTeX)\n(\\else)\n(\\pdfoutput=1 % we are running PDFLaTeX)\n(\\pdftrue)\n(\\fi)\n/%$1\n%$2\n%$3\n%$4\n%$5\n%$6\n%$7\n\\RequirePackage\{ifpdf\}\n/;
+
+# add "CJKbookmarks" parameter to command usepackage{hyperref}
+s/(\\ifpdf)\n(\\usepackage\[pdftex,bookmarksnumbered,colorlinks,backref,bookmarks,breaklinks,linktocpage,plainpages=false,pdfstartview=FitH\]\{hyperref\})\n(\\else)\n(\\usepackage\[bookmarksnumbered,colorlinks,backref,bookmarks,breaklinks,linktocpage,plainpages=false,\]\{hyperref\})\n(\\fi)\n/$1\n\\usepackage\[pdftex,CJKbookmarks,bookmarksnumbered,colorlinks,backref,bookmarks,breaklinks,linktocpage,plainpages=false,pdfstartview=FitH\]\{hyperref\}\n$3\n$4\n$5\n/;
+
+# add "Copyright" for chinese version
+s/(\\tableofcontents)/\n\n\\begin\{center\}Copyright \\copyright\{\} 2008-2015 本文档中文版权归sunguonian\@gmail.com所有\\end\{center\}\n\\begin\{center\}\n\n允许任何个人、任何组织以任何目的、任何形式付费或免费使用，拷贝，修改和分发\n本文档，前提是需要在所有拷贝中出现上述的版权声明和本许可声明。\n\\end\{center\}\n\n$1/;
+
+# replace 10pt font with 11pt font. line 19/21 in Bv9ARM-gbk.tex
+# for ASCII characters, 11pt is too large
+# need to enlarge chinese characters only.
+#s/,10(pt,twoside,openright,\]\{report\})/,11$1/g;
+
+# handle char 0xd8(216, \330) at A.1.1, "Oivind Kure"
+s/\xd8ivind Kure/\{\\O\}ivind Kure/;
+
+# expand bookmark for Appendix B
+s/\\section\K\*\{(?!(Name|\\docbook))/\{/g;
+s/\\section\*\{\KName/名字/g;
+s/\\subsection\*\{\KSynopsis/概要/g;
+
+# handle NOTE/WARNING box
+s/\{Note\}/\{注意\}/g;
+s/\{Warning\}/\{警告\}/g;
+
+print "$_";
---
+++ branches/diff_bv9arm.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+
+my($v1) = shift;
+my($v2) = shift;
+print "$v1, $v2\n";
+
+my($norm1) = norm($v1);
+my($norm2) = norm($v2);
+print "$norm1, $norm2\n";
+
+my($file0) = join('_', 'di', $norm1, $norm2, '00');
+print "$file0\n";
+
+`diff -C 6 -r $v1 $v2 > $file0`;
+
+my($file1) = join('_', 'di', $norm1, $norm2, '01');
+print "$file1\n";
+
+open(FRD, "<$file0");
+open(FWR, ">$file1");
+while(<FRD>) {
+	chomp;
+	if(/^diff / and (/\.(docbook|xml)$/)) {
+		print FWR "$_\n";
+	}
+}
+close(FRD);
+close(FWR);
+
+chmod(0755, $file1);
+
+my($file2) = join('_', 'di', $norm1, $norm2, '02');
+print "$file2\n";
+`./$file1 > $file2`;
+
+sub norm {
+	my($name) = @_;
+	$name =~ s/^bind\-//;
+	$name =~ s/(\.|\-)//g;
+	$name =~ tr/A-Z/a-z/;
+	return($name);
+}

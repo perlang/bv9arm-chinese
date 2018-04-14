7689a7dd50e751ec16088bc606b37e8d8f47dee5
SUN Guonian <diancn@sina.com>
2018-04-14 20:21:29 +0800

    works about new building methed about Docbook 5.0

--- branches/9.11.0a3/add_cjk_tex.pl
+++ branches/9.11.0a3/add_cjk_tex.pl
@@ -1,31 +1,17 @@
 #!/usr/bin/perl
 
 while(<>) {
-  chomp;
-  if(/^\\begin\{document\}$/) {
-    print <<EOB;
+	chomp;
+	if(/^\\begin\{document\}$/) {
+		print <<EOB;
 
 \% Handle the lines with both chinese characters and ASCII.
-\\sloppy
-
-\\usepackage{CJK}
-\%\\usepackage[dvips,CJKbookmarks]{hyperref}
+\%\\sloppy
 
 \\begin{document}
 
-%\\begin{CJK*}{GBK}{fang}
-\\begin{CJK*}{GB}{gbsn}
-\\CJKtilde
-
 EOB
-    next;
-  }
-  elsif(/^\\end\{document\}$/) {
-    print <<EOB;
-\\end{CJK*}
-\\end{document}
-EOB
-    next;
-  }
-  print "$_\n";
+		next;
+	}
+	print "$_\n";
 }

b58f5f2d96622d0d22376233c7cb7ee69d1b3104
perlang <diancn@sina.com>
2020-02-22 23:34:16 +0800

    add 9.14.7

---
+++ branches/9.14.7/add_xelatex.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+
+while(<>) {
+	chomp;
+	print "$_\n";
+	if(/^\\documentclass\{book\}/) {
+		print "\\usepackage\{xeCJK\}\n";
+	}
+	if(/^\\makeglossary/) {
+		print <<EOF
+
+\\setCJKmainfont[BoldFont={文泉驿微米黑},ItalicFont={AR PL UKai CN}]{AR PL UMing CN}
+\\setCJKsansfont{文泉驿微米黑}
+
+\\setCJKfamilyfont{zhsong}{Source Han Serif CN Light}
+\\setCJKfamilyfont{zhhei}{Source Han Sans CN Medium}
+\\setCJKfamilyfont{zhkai}{STKaiti}
+    
+
+\\newcommand*{\\songti}{\\CJKfamily{zhsong}} % 宋体
+\\newcommand*{\\heiti}{\\CJKfamily{zhhei}}   % 黑体
+\\newcommand*{\\kaiti}{\\CJKfamily{zhkai}}   % 楷体
+
+EOF
+	}
+	if(/^\\begin\{document\}/) {
+		print "\\songti\n\n";
+	}
+}
